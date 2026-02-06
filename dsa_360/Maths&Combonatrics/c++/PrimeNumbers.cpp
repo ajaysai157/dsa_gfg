b571419334c66178ecdc7bbed7546efbb4731a21
Ajaysai157 <bhargavchonga976@gmail.com>
2026-02-06 22:40:59 +0530

    Checking wether prime or not

---
+++ dsa_360/Maths&Combonatrics/c++/PrimeNumbers.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    bool isPrime(int n)
+    {
+        if (n <= 1)
+            return false;
+        for (int i = 2; i <= sqrt(n); i++)
+        {
+            if (n % i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    Solution obj;
+    if (obj.isPrime(n))
+    {
+        cout << "Prime" << endl;
+    }
+    else
+    {
+        cout << "Not Prime" << endl;
+    }
+
+    return 0;
+}
